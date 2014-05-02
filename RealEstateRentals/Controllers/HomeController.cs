594b42e4f25cd1468631a948fc9203728978da36
Leon Rich <leoncrich@gmail.com>
2014-05-02 11:25:34 -0400

    First Push
    First Push

---
+++ RealEstateRentals/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstateRentals.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //Search Listings
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+       
+    }
+}
---
+++ RealEstateRentals/ViewModels/ListingSearchVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealEstateRentals.ViewModels
+{
+    public class ListingSearchVM
+    {
+        public string City { get; set; }
+        public int NumberOfBedrooms { get; set; }
+        public int Price { get; set; }
+
+        List<ListingSearchListingsVM> lListings {get; set;} 
+    }
+
+    public class ListingSearchListingsVM
+    {
+        public int ListingID { get; set; }
+        public string Address { get; set; }
+    }
+}
